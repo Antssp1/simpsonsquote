ac788b0d751027f8034a0c6816f5e6fa2466cf6f
Arthur Thill <arthur.thill17.at@gmail.com>
2018-11-08 16:10:51 +0100

    simpsons

--- src/Lamp.js
+++ src/Lamp.js
@@ -2,11 +2,20 @@
 import React, { Component } from 'react';
 
 class Lamp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: true
+    };
+  }
+  handleClick = () => {
+    this.setState({ on: !this.state.on });
+  };
   render() {
-    const light = this.props.on ? 'on' : 'off';
+    const light = this.state.on ? 'on' : 'off';
     return (
       <div className="Lamp">
-        <button className={light}>{light.toUpperCase()}</button>
+        <button onClick={() => this.handleClick()} className={light}>{light.toUpperCase()}</button>
         <figure className={light} />
       </div>
     );
